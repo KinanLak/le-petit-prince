62b620f188a7854c856582b0da52c4dbbe8566b2
Kinan LAKHDAR <kinan.lakh@gmail.com>
2025-03-21 01:35:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Le petit prince',
+  webDir: 'www'
+};
+
+export default config;
